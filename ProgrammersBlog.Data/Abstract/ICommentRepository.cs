43f902ace297f8f7f6f01b0f146fa86418b5f162
Furkan Kocer <furkankocer.tr@gmail.com>
2020-12-10 21:12:26 +0300

    10.12.2020

--- ProgrammersBlog.Data/Abstract/ICommentRepository.cs
+++ ProgrammersBlog.Data/Abstract/ICommentRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Abstract;
+
 namespace ProgrammersBlog.Data.Abstract
 {
-    public interface ICommentRepository
+    public interface ICommentRepository : IEntitiyRepository<Comment>
     {
     }
 }
---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfCommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
+{
+    public class EfCommentRepository : EfEntityRepositoryBase<Comment>, ICommentRepository
+    {
+        public EfCommentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
