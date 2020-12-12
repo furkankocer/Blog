196aa921eeb30ed6d0254edf9af6dc3f7aa9d373
furkankocer <furkankocer@hotmail.com.tr>
2020-12-12 16:20:14 +0300

    UnitOfWork pattern eklendi

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+
+        Task<int> SaveAsync();
+    }
+} 
