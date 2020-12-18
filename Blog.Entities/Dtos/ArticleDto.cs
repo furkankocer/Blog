6f2639249d8009d7f9300508589a3b7fd77693e2
furkankocer <furkankocer@hotmail.com.tr>
2020-12-18 19:41:32 +0300

    Dto eklemesi

--- Blog.Entities/Dtos/ArticleDto.cs
+++ Blog.Entities/Dtos/ArticleDto.cs
@@ -1,7 +1,12 @@
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.Entities.Concrete;
+
 namespace Blog.Entities.Dtos
 {
     public class ArticleDto
     {
+        public Article Article { get; set; }
+        public ResultStatus ResultStatus { get; set; }
         
     }
 }
--- Blog.Entities/Dtos/ArticleListDto.cs
+++ Blog.Entities/Dtos/ArticleListDto.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.Entities.Concrete;
+
 namespace Blog.Entities.Dtos
 {
     public class ArticleListDto
     {
-        
+        public IList<Article> Articles { get; set; }
     }
 }
